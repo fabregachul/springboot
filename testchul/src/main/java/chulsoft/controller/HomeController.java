717137844341bf6ac9a521ce54e16f18add219ee
fabregachul <duscnf1212@naver.com>
2020-04-23 16:21:19 +0900

    JPA로 간단 API 만들기 실습(Postman을 이용한 테스트데이터 DB적재 실습)

--- testchul/src/main/java/chulsoft/controller/HomeController.java
+++ testchul/src/main/java/chulsoft/controller/HomeController.java
@@ -1,11 +1,16 @@
 package chulsoft.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import chulsoft.domain.posts.PostsRepository;
+import chulsoft.dto.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 public class HomeController {
+
+    private PostsRepository postsRepository;
+
     @RequestMapping("/qkrduscnf")
     public String index(){
         return "확인!";
@@ -14,5 +19,10 @@ public class HomeController {
     @GetMapping("/hello")
     public String hello(){
         return "HelloWorld!!!";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
